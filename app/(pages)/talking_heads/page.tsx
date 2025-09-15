b013076b548d9341ef0915ece6eab50db6af8a2a
TheRealBiggySmalls <nicholassujecki@gmail.com>
2025-09-15 16:03:32 +0200

    temp head

--- app/(pages)/talking_heads/page.tsx
+++ app/(pages)/talking_heads/page.tsx
@@ -2,7 +2,24 @@ import { WigglyText } from '@/app/components/ui/WigglyText';
 
 export default function TalkingHeads() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-8">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          style={{
+            width: '50%',
+            height: '50%',
+            display: 'block',
+            objectFit: 'contain',
+            mixBlendMode: 'screen',
+            WebkitMaskImage: '-webkit-radial-gradient(white, black)', 
+          }}
+        >
+          <source src="/3d/natty2.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
         <div className="text-3xl font-bold">
           <WigglyText 
             text="coming soon..." 
